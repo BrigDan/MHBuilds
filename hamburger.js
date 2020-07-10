568f2bddf4338ec65828140fad1a427397d73c32
Brigdan <kacperdev@gmail.com>
2020-07-10 13:15:48 +0100

    Inital commit

---
+++ hamburger.js
@@ -0,0 +1,23 @@
+const toggle = document.querySelector(".toggle");
+const logo = document.querySelector(".logo");
+const menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+        logo.querySelector("a").innerHTML = "<h1 class='logo-text'></h1>";
+
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
